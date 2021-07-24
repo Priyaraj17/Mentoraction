0eefe32d01916bfe1d58172fb6b00aaa70efffb9
Priyaraj17 <priyaraj.17@outlook.com>
2021-07-25 01:23:02 +0530

    UI updated

--- routes/views.js
+++ routes/views.js
@@ -6,20 +6,9 @@ const router = express.Router();
 
 router.get("/", viewsController.getOverview);
 router.get("/page", viewsController.getPage);
-router.get(
-  "/mentor/:slug",
-  authController.isLoggedIn,
-  viewsController.getMentor
-);
+router.get("/mentor/:slug", viewsController.getMentor);
 router.get("/recentChats", viewsController.getRecentChats);
 router.get("/login", viewsController.getLoginForm);
 router.get("/signup", viewsController.getSignUpForm);
 router.get("/me", authController.protect, viewsController.getAccount);
-
-// router.post(
-//   "/submit-user-data",
-//   authController.protect,
-//   viewsController.updateUserData
-// );
-
 module.exports = router;
