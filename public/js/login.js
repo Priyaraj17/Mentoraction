6b73066fc95c0ef6626d055555a1e9e909a7e4ca
Priyaraj17 <priyaraj.17@outlook.com>
2021-07-25 19:44:05 +0530

    Deployment Ready

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: "POST",
-      url: "http://127.0.0.1:3000/api/v1/users/login",
+      url: "/api/v1/users/login",
       data: {
         email,
         password,
@@ -40,7 +40,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: "GET",
-      url: "http://127.0.0.1:3000/api/v1/users/logout",
+      url: "/api/v1/users/logout",
     });
     showAlert("success", "Logging you out");
     if ((res.data.status = "success")) location.reload(true);
@@ -88,7 +88,7 @@ export const signup = async (
   try {
     const res = await axios({
       method: "POST",
-      url: "http://127.0.0.1:3000/api/v1/users/signup",
+      url: "/api/v1/users/signup",
       data: {
         firstName,
         lastName,
